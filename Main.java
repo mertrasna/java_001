0cdf1279ac7345ac4a46a74695a654e505dc6f8d
Bahar Onguru <baharonguru@gmail.com>
2025-05-19 00:22:27 +0200

    added method addSum

--- Main.java
+++ Main.java
@@ -1,5 +1,15 @@
 public class Main {
+    static int addSum(int a,int b){
+        int c = a+b;
+        return c;
+    }
+
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        int x = 7;
+        int y = 27;
+        int result = addSum(x, y);
+        System.out.println(result);
     }
 }
